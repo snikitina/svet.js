b0b7a36358943eb3f357dc71fdc965cbfe05400a
Christopher Turner <christopher.turner@mi9.com.au>
2017-06-23 16:11:47 +1000

    changing location of README

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const file = fs.readFileSync('./README.md').toString();
+const file = fs.readFileSync(`${__dirname}/README.md`).toString();
 
 const TOKEN = '## Comments and Wishes';
 const commentsAndWishes = file.split(TOKEN)[1];
